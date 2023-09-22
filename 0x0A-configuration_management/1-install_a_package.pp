53951b4aed72cfde8d299673bed23698ec22f501
TC751 <teddychiingujr@gmail.com>
2023-09-22 11:01:17 -0700

    file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install flask
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin',
+  creates     => '/usr/local/lib/python3.x/dist-packages/Flask-2.1.0.dist-info',
 }
+
