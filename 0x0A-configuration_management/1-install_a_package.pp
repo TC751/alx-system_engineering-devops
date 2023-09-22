311003f368be603a340810ead1e0e45d931968dd
TC751 <teddychiingujr@gmail.com>
2023-09-22 11:06:07 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install flask
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin',
-  creates     => '/usr/local/lib/python3.x/dist-packages/Flask-2.1.0.dist-info',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+
 
